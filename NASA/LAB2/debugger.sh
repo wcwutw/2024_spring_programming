59570866a94d2ccc0bc54c652a01849f41d133ff
wcwu <103374825+wcwutw@users.noreply.github.com>
2024-02-28 00:13:20 +0800

    upload LAB2 AC code

--- NASA/LAB2/debugger.sh
+++ NASA/LAB2/debugger.sh
@@ -1,28 +1,39 @@
 #!/usr/bin/env bash
 # $1=gen.c $2=a.c $3=b.c $4=time
 
+
 gcc -o gen "$1"
 gcc -o a "$2"
 gcc -o b "$3"
 time="$4"
 
 for ((i=1; i<=$time; i++)) do
+	#in=$(./gen $i)
+	#output_a=$(echo "$in" | ./a)
+	#output_b=$(echo "$in" | ./b)
 	./gen $i > in.txt
+	./a < in.txt > outputa.txt
+	./b < in.txt > outputb.txt
 	output_a=$(./a < in.txt)
 	output_b=$(./b < in.txt)
-	if [ "$output_a" != "$output_b" ]; then
+	if diff outputa.txt outputb.txt >useless.txt; then
+		:
+	else
 		echo "Test $i:"
 		echo "Input:"
 		echo "--------------------"
+		#echo "$in"
 		cat in.txt
 		echo "--------------------"
-		echo "Output of sol.c:"
+		echo "Output of $2:"
 		echo "--------------------"
-		echo "$output_a"
+		cat outputa.txt
+		#echo "$output_a"
 		echo "--------------------"
-		echo "Output of wa.c:"
+		echo "Output of $3:"
 		echo "--------------------"
-		echo "$output_b"
+		cat outputb.txt
+		#echo "$output_b"
 		echo "--------------------"
 		exit 1
 	fi
