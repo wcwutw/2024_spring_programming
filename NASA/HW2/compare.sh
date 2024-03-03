344b8d9acd28164379a87d86716248130227001f
wcwu <103374825+wcwutw@users.noreply.github.com>
2024-03-03 22:14:13 +0800

    upload solution (3/3 22:14)

--- NASA/HW2/compare.sh
+++ NASA/HW2/compare.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+#set -x
+
 print_help() {
 	echo "usage: ./compare.sh [OPTION] <PATH A> <PATH B>"
 	echo "options:"
@@ -84,7 +86,10 @@ check() {
 
 	path_a="$1"
 	path_b="$2"
-	
+	if [[ "$ropt" == "false" && ( "$nopt" == "true" || "$aopt" == "true" ) ]]; then
+		print_help
+		exit 1
+	fi
 	if [[ "$ropt" == "true" && (! -d "$path_a" || ! -d "$path_b") ]]; then
 		#echo "-r but not both directory(err 5)"
 		print_help
@@ -102,11 +107,52 @@ check() {
 		print_help
 		exit 1
 	fi
-
 	#echo "accepted"
 }
 
+cmp_sym() {
+	if [[ "$lopt" == "true" ]]; then
+		if [[ -L "$1" ]]; then
+			tar1=$(readlink "$1")
+		fi
+		if [[ -L "$2" ]]; then
+			tar2=$(readlink "$2")
+		fi
+		if [[ ( ! -L "$1" ) || ( ! -L "$2" ) || "$tar1" != "$tar2" ]]; then
+			echo "changed 100%"
+		else
+			echo "changed 0%"
+		fi
+	else
+		if [[ -L "$1" ]]; then
+			if [[ -L "$2" ]]; then
+				echo "changed 0%"
+			else
+				echo "create" # $2
+			fi
+		elif [[ -L "$2" ]]; then
+			echo "delete"	# $1
+		fi
+	fi
+}
+
 cmp_file() {
+	if [[ "$lopt" == "true" ]]; then
+		if [[  -L "$1" || -L "$2" ]]; then
+			local result=$(cmp_sym "$1" "$2")
+			if [[ $result != "changed 0%" ]]; then
+				if [[ "$result" == "delete" ]]; then
+					echo "$1 delete"
+				elif [[ "$result" == "create" ]]; then
+					echo "$2 create"
+				else
+					echo "$result"
+				fi
+			fi
+			return
+		fi
+	fi
+
 	local diffo_output=$(diff -d "$1" "$2")
 	local diff_output=$(diff -d -u "$1" "$2" | grep -vE '^(---|\+\+\+|@@)' 2>&1)
 	local result=$(echo "$diffo_output")
@@ -116,10 +162,12 @@ cmp_file() {
 	else
 		local a=$(echo "$diff_output" | grep -cE "^\-")
 		local b=$(echo "$diff_output" | grep -cE "^\+")
-		local c=$(echo "$diff_output" | grep -cE "^ ")
-		local tota=$(wc -l < "$1");
+		# local c=$(echo "$diff_output" | grep -cE "^ ")
+		#local tota=$(wc -l < "$1");
+		local tota=$(grep -a '^' "$1" | wc -l)
+		#echo $diff_output
 		#echo "$a:$b:$c"
-		#local c=$((tota-a))
+		local c=$((tota-a))
 		local max=$((b > a ? b : a))
 		if [[ $max == 0 && $c == 0 ]]; then
 			x=0
@@ -131,18 +179,9 @@ cmp_file() {
 	echo "changed ${x}%"
 }
 
-cmp_sym() {
-	tar1=$(readlink "$1")
-	tar2=$(readlink "$2")
-	if [[ "$tar1" != "$tar2" || ( ! -L "$2" ) || ( ! -L "$1" ) ]]; then
-		echo "changed 100%"
-	else
-		echo "changed 0%"
-	fi
-}
-
 cmp_dir() {
 	declare -a file_status=()
+
 	if [[ "$lopt" == "true" ]]; then
 		local symlinks=$(find "$1"/* -type l)
 		for symlink1 in $symlinks; do
@@ -155,7 +194,15 @@ cmp_dir() {
 						local result=$(cmp_sym "$symlink1" "$symlink2")
 						if [[ $result != "changed 0%" ]]; then
 							local now=$(basename "$symlink1");
-							file_status+=("$now: $result");
+							local now2=$(basename "$symlink2");
+
+							if [[ "$result" == "delete" ]]; then
+								file_status+=("$now delete")
+							elif [[ "$result" == "create" ]]; then
+								file_status+=("$now2 create")
+							else
+								file_status+=("$now: $result")
+							fi
 						fi
 					else 
 						file_status+=("${symlink1#$1/} delete");
@@ -169,7 +216,7 @@ cmp_dir() {
 				if [ -L "$symlink2" ]; then
 					symlink1="$1/$(basename "$symlink2")"
 					if [[ ! -e "$symlink1" ]]; then
-						file_status+=("${symlink2#$2/} create");
+						file_status+=("${symlink2#$2/} create")
 					fi
 				fi
 			fi
@@ -180,18 +227,35 @@ cmp_dir() {
 		rel_path="${file1#$1/}"
 		file2="$2/$rel_path"
 		path_to_file="$(dirname $file1)"
-		areal="false" #not hidden
-		while [[ "$path_to_file" != "/" ]]; do
-			if [[ "$(basename "$path_to_file")" == .* ]]; then
-				areal="true"
-				break
-			fi
-			path_to_file="$(dirname "$path_to_file")"
-		done
-		echo "$areal: $file1 / "$(basename "${file1#$1/}")""
+		if [[ "./$rel_path" == *"/."* ]]; then
+			areal="true" #hidden
+		else
+			areal="false" #not hidden
+		fi
+		# echo "---" $rel_path
+#		while [[ "$path_to_file" != "/" ]]; do
+#			if [[ "$(basename "$path_to_file")" == .* ]]; then
+#				areal="true"
+#				break
+#			fi
+#			path_to_file="$(dirname "$path_to_file")"
+#		done
+		# echo "$areal: $file1 / "$(basename "${file1#$1/}")""
 		if [[ ( ( "$aopt" == "false" && "$areal" == "false") || ( "$aopt" == "true" ) ) && ( ( "$(basename "${file1#$1/}")" =~ "$exp" && "$nopt" == "true" ) || ( "$nopt" == "false" ) ) ]]; then
-			if [[ ! -e "$file2" || "$(basename "${file2#$2/}")" == .* || -L "$file2" ]]; then
-				file_status+=("$rel_path delete");
+			if [[ -L "$file1" || -L "$file2" ]]; then
+				local result=$(cmp_sym "$file1" "$file2")
+				if [[ $result != "changed 0%" ]]; then
+					if [[ "$result" == "delete" ]]; then
+						file_status+=("$rel_path delete")
+					elif [[ "$result" == "create" ]]; then
+						rel_path2="${file2#$1/}"
+						file_status+=("$rel_path2 create")
+					else
+						file_status+=("$rel_path: $result")
+					fi
+				fi
+			elif [[ ! -e "$file2" || "$(basename "${file2#$2/}")" == .* || -L "$file2" ]]; then
+				file_status+=("$rel_path delete")
 			else
 				local result=$(cmp_file "$file1" "$file2")
 				if [[ $result != "changed 0%" ]]; then
