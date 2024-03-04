4a28d3636c15f64c2d50e6806451513e02e6156d
wcwu <103374825+wcwutw@users.noreply.github.com>
2024-03-04 22:58:49 +0800

    upload final solution

--- NASA/HW2/compare.sh
+++ NASA/HW2/compare.sh
@@ -24,32 +24,25 @@ path_b=""
 check() {
 	local size="$#"
 	if [[ "$size" -lt 2 ]]; then
-		#echo "less than two input(err 8)"
 		print_help
 		exit 1
 	fi
-
 	for arg in "${@: -2}"; do
 		if [ ! -e "$arg" ]; then
-			#echo "<file A> or <file B> not found in the directory"
 			print_help
 			exit 1
 		fi
-
 		if [ ! -d "$arg" ] && [ ! -f "$arg" ] && [ ! -L "$arg" ]; then
-			#echo "last two isn't eligible path(err 7 or err 8)"
 			print_help
 			exit 1
 		fi
 	done
-
 	while getopts ":ahlrn:?" opt; do
 		case $opt in
 			a)
 				aopt=true
 				;;
 			h)
-				#echo "input -h(err 1)"
 				print_help
 				exit 1
 				;;
@@ -61,7 +54,6 @@ check() {
 				if [[ "$OPTARG" =~ "$exp" ]]; then
 					exp="$OPTARG"
 				else
-					#echo "regex didn't come after -n(err 3)"
 					print_help
 					exit 1
 				fi
@@ -70,22 +62,17 @@ check() {
 				ropt=true
 				;;
 			\?)
-				#echo "parameter not found(err 2)"
 				print_help
 				exit 1
 				;;
 		esac
 	done
-
 	shift $((OPTIND -1))
 	size="$#"
-
 	if [[ "$size" -ne 2 ]]; then
-		#echo "read something useless(err 9)"
 		print_help
 		exit 1
 	fi
-
 	path_a="$1"
 	path_b="$2"
 	if [[ "$ropt" == "false" && ( "$nopt" == "true" || "$aopt" == "true" ) ]]; then
@@ -93,19 +80,18 @@ check() {
 		exit 1
 	fi
 	if [[ "$ropt" == "true" && (! -d "$path_a" || ! -d "$path_b") ]]; then
-		#echo "-r but not both directory(err 5)"
 		print_help
 		exit 1
 	fi
-	
 	if [[ "$ropt" == "false" && ( -d "$path_a" || -d "$path_b") ]]; then
-		#echo "not -r but contain directory(err 6)"
-		print_help
-		exit 1
+		if [[ ( -L "$path_a" && -f "$path_b") || ( -f "$path_a" && -L "$path_b" ) ]]; then
+			:
+		else
+			print_help
+			exit 1
+		fi
 	fi
-	
 	if [[ "$lopt" == "false" && ( -L "$path_a" || -L "$path_b") ]]; then
-		#echo "contain symb link but don't have -l"
 		print_help
 		exit 1
 	fi
@@ -140,16 +126,12 @@ cmp_file() {
 	if [[ ! -n "$result" ]]; then
 		return
 	fi
-	#echo "$result"
-	if [[ $result == *"Binary files"* || $result == *"‰∫åÂÖÉÁ¢ºÊ™î"* || $result == *"symbolic link"* || $result == *"Symbolic link"* || $result == *"Á¨¶ËôüÈÄ£Áµê"* ]]; then 
+	if [[ $result == *"Binary files"* || $result == *"§G§∏ΩX¿…"* || $result == *"symbolic link"* || $result == *"Symbolic link"* || $result == *"≤≈∏π≥sµ≤"* ]]; then 
 		x=100
 	else
 		local a=$(echo "$diff_output" | grep -c "^-" )
 		local b=$(echo "$diff_output" | grep -c "^+" )
-		#local c=$(echo "$diff_output" | egrep -c "^ ")
-		#local tota=$(wc -l < "$1");
 		local tota=$(grep -a "^" "$1" | wc -l)
-		#echo $diff_output
 		local c=$((tota-a))
 		local max=$((b > a ? b : a))
 		if [[ $max == 0 && $c == 0 ]]; then
@@ -158,13 +140,11 @@ cmp_file() {
 			x=$((100 * max / (max + c)))
 		fi
 	fi
-
 	echo "changed ${x}%"
 }
 
 cmp_dir() {
 	declare -a file_status=()
-	
 	acom=""
 	lcom="-type f"
 	if [[ "$aopt" == "false" ]]; then
@@ -177,7 +157,6 @@ cmp_dir() {
 	ec2="cd $2 && find ./ $lcom $acom"
 	en1=$(eval "$ec1")
 	en2=$(eval "$ec2")
-	
 	while IFS= read -r file1; do
 		rel_path="${file1#\./}"
 		fa="$1/""$rel_path"
@@ -195,7 +174,6 @@ cmp_dir() {
 			file_status+=("$rel_path delete")
 		fi
 	done < <(echo "$en1")
-	
 	while IFS= read -r file2; do
 		rel_path="${file2#\./}"
 		fa="$1/""$rel_path"
@@ -208,10 +186,8 @@ cmp_dir() {
 			file_status+=("$rel_path create")
 		fi
 	done < <(echo "$en2")
-	
 	LC_ALL=C
 	IFS=$'\n' sorted_status=($(sort <<<"${file_status[*]}"))
-
 	for status in "${sorted_status[@]}"; do
 		case $status in
 			*create)
@@ -249,4 +225,3 @@ elif diff -qr "$path_a" "$path_b" >/dev/null; then
 else
 	cmp_file "$path_a" "$path_b"
 fi
-
